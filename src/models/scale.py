b1fc32db92af015bf576c9158cfc4638d653a890
arsenaultk9 <arsenaultk9@gmail.com>
2023-01-11 18:12:57 -0500

    First version of scale filter.

---
+++ src/models/scale.py
@@ -0,0 +1,58 @@
+from typing import List
+from src.models.notes import notes
+
+# Interval position by mode
+mode_intervals = {
+    'major': [0, 2, 4, 5, 7, 9, 11],
+    'minor': [0, 2, 3, 5, 7, 8, 10]
+}
+
+def get_scale_notes(root: str, mode=str):
+    scale_notes = []
+
+    mode_interval = mode_intervals[mode]
+    start_index = notes.index(root)
+
+    for interval_pos in mode_interval:
+        cur_index = start_index + interval_pos
+        cur_index = cur_index if cur_index < 12 else cur_index - 12
+        
+        note = notes[cur_index]
+        scale_notes.append(note)
+
+
+    return scale_notes
+
+def get_note_in_bound(notes: List[str], note_index: int):
+    if note_index < len(notes):
+        return notes[note_index]
+
+    index_from_bound = note_index - len(notes)
+    return notes[index_from_bound]
+
+class Scale:
+    def __init__(self, root: str, mode='major'):
+        self.root = root
+        self.mode = mode
+        self.notes = get_scale_notes(root, mode)
+
+
+    def get_main_triads(self):
+        triad_chords = []
+
+        for note_index in range(len(self.notes)):
+            note_a = get_note_in_bound(self.notes, note_index)
+            note_b = get_note_in_bound(self.notes, note_index + 2)
+            note_c = get_note_in_bound(self.notes, note_index + 4)
+
+            chord_suffix = ''.join(sorted([note_a, note_b, note_c]))
+            triad_chords.append(chord_suffix)
+
+        return triad_chords
+
+
+    def __str__(self):
+        return f'root: {self.root}, mode: {self.mode}, notes: [ {", ".join(self.notes)} ]'
+
+    def __repr__(self):
+        return self.__str__()
---
+++ src/visualization/scale_color_filter.py
@@ -0,0 +1,12 @@
+from src.models.scale import Scale
+
+
+def get_scale_triads_filter(chord_suffixes, scale: Scale):
+    colors = []
+    scale_chords = scale.get_main_triads()
+
+    for chord_suffix in chord_suffixes:
+        color = 'red' if chord_suffix in scale_chords else 'black'
+        colors.append(color)
+
+    return colors
