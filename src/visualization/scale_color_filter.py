44ccc39a4e27dffb45f87a9a2cf4cdcc46dec4f3
arsenaultk9 <arsenaultk9@gmail.com>
2023-01-11 18:27:13 -0500

    Better visualisation of scale triads

--- src/visualization/scale_color_filter.py
+++ src/visualization/scale_color_filter.py
@@ -6,7 +6,7 @@ def get_scale_triads_filter(chord_suffixes, scale: Scale):
     scale_chords = scale.get_main_triads()
 
     for chord_suffix in chord_suffixes:
-        color = 'red' if chord_suffix in scale_chords else 'black'
+        color = 'red' if chord_suffix in scale_chords else 'lightgrey'
         colors.append(color)
 
     return colors
