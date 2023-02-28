8a7877e4002fb421673d785ecd16e3553b665073
arsenaultk9 <arsenaultk9@gmail.com>
2023-02-28 08:25:38 -0500

    Start of random forest classifier.

--- src/generation_data_loader.py
+++ src/generation_data_loader.py
@@ -3,6 +3,8 @@ import pickle
 import src.constants as constants
 
 from src.models.generation_dataset import GenerationDataset
+from src.generation_dataset_builder import get_input_and_targets
+
 
 def load_data():
     with open(constants.GENERATION_TRAINING_DATA_PATH, 'rb') as file:
@@ -14,3 +16,14 @@ def load_generation_data():
 
     vocabulary = data['chords_vocabulary']
     return (vocabulary, GenerationDataset(data['train']), GenerationDataset(data['valid']), GenerationDataset(data['test']))
+
+
+def load_random_forest_data():
+    data = load_data()
+
+    vocabulary = data['chords_vocabulary']
+    _, X_train, y_train = get_input_and_targets(data['train'] + data['valid'])
+    _, X_test, y_test = get_input_and_targets(data['test'])
+
+    return (vocabulary, X_train, y_train, X_test, y_test)
+
---
+++ train_random_forest.py
@@ -0,0 +1,41 @@
+import random
+import torch
+import torch.optim as optim
+
+from torch.utils.data import DataLoader
+
+import src.midi_generator as midi_generator
+import src.note_generator as note_generator
+import src.constants as constants
+
+from src.generation_data_loader import load_random_forest_data
+from src.networks.lstm_embedding_network import LstmEmbeddingNetwork
+from src.networks.lstm_vanilla_network import LstmVanillaNetwork
+from src.network_trainer import NetworkTrainer
+from src.network_sequence_generator import NetworkSequenceGenerator
+
+vocabulary, X_train, y_train, X_test, y_test = load_random_forest_data()
+
+# train
+print(len(X_train))
+
+# # === Save model for production use ===
+# (_, x_sequence, y_pred) = train_dataset[0:constants.BATCH_SIZE]
+# (h, c) = network.get_initial_hidden_context()
+
+# traced_script_module = torch.jit.trace(network.forward, (x_sequence.to(device), (h, c)))
+# traced_script_module.save("result_model/generation_network.pt")
+
+# # ==== Code to generate to midi. ====
+# random_seeds = random.sample(range(0, len(test_dataset) - constants.BATCH_SIZE), 9)
+
+# for file_index, song_index in enumerate(random_seeds):
+#     print(f'Generating song {file_index + 1}')
+
+#     sequence_generator = NetworkSequenceGenerator(network)
+#     (_, x_sequence, y_pred) = test_dataset[song_index:song_index+constants.BATCH_SIZE]
+
+#     generated_sequence = sequence_generator.generate_sequence(x_sequence)
+
+#     generated_note_infos = note_generator.generate_note_info(generated_sequence, vocabulary)
+#     midi_generator.generate_midi(f'generated_file{file_index}.mid', generated_note_infos)
